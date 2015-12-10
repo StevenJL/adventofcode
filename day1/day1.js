ef3952cc31a6cb04d6e1582bc473b4fa1111054d
Steven Li <sli@bleacherreport.com>
2015-12-10 15:26:28 -0800

    first commit

---
+++ day1/day1.js
@@ -0,0 +1,16 @@
+// node day1.js 
+
+floor = 0;
+parens_str = require('fs').readFileSync('./parens.dat').toString();
+
+for(var indx = 0; indx < parens_str.length; indx++)
+{
+  if(parens_str[indx] == '(') {
+    floor = floor + 1; 
+  } else if (parens_str[indx] == ')') {
+    floor = floor - 1;
+  }
+}
+
+console.log(floor);
+
